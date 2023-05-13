3e5436e175e4bb47e0024ea1b67320df9dbd1a2b
juyeong525 <juyeongno1@naver.com>
2023-05-14 04:15:14 +0900

    ✨ :: LocalDataSource

---
+++ Services/AuthService/Sources/Data/Local/DataSource/LoaclTokenDataSourceImpl.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+import KeychainModule
+
+class LoaclTokenDataSourceImpl: LoaclTokenDataSource {
+
+    let keychain = KeychainModule<KeychainTypeForAuth>()
+
+    init() {
+        keychain.accessGroup = "5G56CAU886.com.xquare.XQUARE-iOS.keychainGroup"
+    }
+
+    func registerAccessToken(_ accessToken: String) {
+        self.keychain.set(accessToken, forKey: .accessToken)
+    }
+
+    func fetchAccessToken() -> String? {
+        self.keychain.get(.accessToken)
+    }
+
+    func registerRefreshToken(_ refreshToken: String) {
+        self.keychain.set(refreshToken, forKey: .refreshToken)
+    }
+
+    func fetchRefreshToken() -> String? {
+        self.keychain.get(.refreshToken)
+    }
+
+    func registerExpiredAt(_ expiredAt: String) {
+        self.keychain.set(expiredAt, forKey: .expiredAt)
+    }
+
+    func fetchExpiredDate() -> Date? {
+        self.keychain.get(.expiredAt)?
+            .toDate(format: .fullDateWithMilliSecondTime)
+    }
+
+}
