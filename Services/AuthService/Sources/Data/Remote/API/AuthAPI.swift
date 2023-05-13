b4b3608c182ec5ee52f1f40a34da7706305cb8b6
juyeong525 <juyeongno1@naver.com>
2023-05-13 17:23:05 +0900

    ⚡️ :: AuthAPI

---
+++ Services/AuthService/Sources/Data/Remote/API/AuthAPI.swift
@@ -0,0 +1,58 @@
+import Foundation
+import Moya
+
+enum AuthAPI: TargetType {
+    case signin(request: SigninRequest)
+//    case signup(request: SignupRequest)
+    case refreshToken(refreshToken: String)
+}
+
+extension AuthAPI {
+
+    var baseURL: URL {
+        return URL(string: "http://43.201.53.240:8000")!
+    }
+
+    var path: String {
+        switch self {
+        case .signin:
+            return "/teachers/auth"
+        case .refreshToken:
+            return "/teachers/token"
+//        case .signup:
+//            return "/teachers/register"
+        }
+    }
+
+    var method: Moya.Method {
+        switch self {
+        case .signin, .refreshToken:
+            return .post
+        }
+    }
+
+    var task: Task {
+        switch self {
+        case .signin(let request):
+            return .requestJSONEncodable(request)
+//        case .signup(let request):
+//            return .requestJSONEncodable(request)
+        default:
+            return .requestPlain
+        }
+    }
+
+    var validationType: ValidationType {
+        return .successCodes
+    }
+
+    var headers: [String: String]? {
+        switch self {
+        case .refreshToken(let refreshToken):
+            return ["Refresh-Token": "Bearer \(refreshToken)"]
+        default:
+            return nil
+        }
+    }
+
+}
