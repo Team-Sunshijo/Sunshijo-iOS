9a511cdc4a0421368afa9443ec15b1a7723384cd
juyeong525 <juyeongno1@naver.com>
2023-05-14 04:24:39 +0900

    ✨ :: SDateUtil 추가

--- Tuist/ProjectDescriptionHelpers/TargetDependency/Modules.swift
+++ Tuist/ProjectDescriptionHelpers/TargetDependency/Modules.swift
@@ -4,6 +4,7 @@ extension TargetDependency {
     
     public struct Module {
         public static let KeychainModule = module(name: "KeychainModule")
+        public static let SDateUtil = module(name: "SDateUtil")
     }
     private static func module(name: String) -> TargetDependency {
         return TargetDependency.project(
