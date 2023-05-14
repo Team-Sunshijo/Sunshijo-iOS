5ab912e7cae4a1a5efae5afb0592b42fa8feaebe
parkdoyeon <parkdoyeon3239@gmail.com>
2023-05-14 23:51:34 +0900

    ♻️ ::	 리팩토링

--- Application/Sources/Scenes/Signup/SignupView.swift
+++ Application/Sources/Scenes/Signup/SignupView.swift
@@ -1,11 +1,3 @@
-//
-//  SignupView.swift
-//  Sunshijo
-//
-//  Created by 박도연 on 2023/05/10.
-//  Copyright © 2023 com.kodomo. All rights reserved.
-//
-
 import SwiftUI
 
 struct SignupView: View {
