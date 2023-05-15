917c447cb792d79225198d8d402a899b12fa0dc5
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-05-15 16:37:13 +0900

    ♻️ :: Lint

--- Application/Sources/extension/shadow.swift
+++ Application/Sources/extension/shadow.swift
@@ -4,5 +4,4 @@ extension View {
     func authSetShadow(color: Color = SunshijoIOSAsset.Colors.main.swiftUIColor, radius: CGFloat = 10) -> some View {
         self.shadow(color: color, radius: radius)
     }
-    
 }
