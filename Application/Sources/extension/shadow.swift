b26f58ce964ee2b61e8b7d9bd9cfb163f361d751
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-05-15 15:30:57 +0900

    ⚡️ :: extension shadow

---
+++ Application/Sources/extension/shadow.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+extension View {
+    func authSetShadow(color: Color = SunshijoIOSAsset.Colors.main.swiftUIColor, radius: CGFloat = 10) -> some View {
+        self.shadow(color: color, radius: radius)
+    }
+    
+}
