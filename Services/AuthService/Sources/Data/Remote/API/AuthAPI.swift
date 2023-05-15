c649248a7ceccdb831e3baee5f50fc83fd12ad54
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-05-16 00:50:40 +0900

    ⚡️ :: AuthAPI Signup추가

--- Services/AuthService/Sources/Data/Remote/API/AuthAPI.swift
+++ Services/AuthService/Sources/Data/Remote/API/AuthAPI.swift
@@ -3,19 +3,22 @@ import Moya
 
 enum AuthAPI: TargetType {
     case signin(request: SigninRequest)
+    case signup(request: SignupRequest)
     case refreshToken(refreshToken: String)
 }
 
 extension AuthAPI {
 
     var baseURL: URL {
-        return URL(string: "http://43.201.53.240:8000")!
+        return URL(string: "http://172.20.10.6:8000")!
     }
 
     var path: String {
         switch self {
         case .signin:
-            return "/teachers/auth"
+            return "/teacher/auth"
+        case .signup:
+                return "/teachers/register"
         case .refreshToken:
             return "/teachers/token"
         }
@@ -23,7 +26,7 @@ extension AuthAPI {
 
     var method: Moya.Method {
         switch self {
-        case .signin, .refreshToken:
+        case .signin, .signup, .refreshToken:
             return .post
         }
     }
@@ -31,6 +34,8 @@ extension AuthAPI {
     var task: Task {
         switch self {
         case .signin(let request):
+            return .requestJSONEncodable(request)
+        case .signup(let request):
             return .requestJSONEncodable(request)
         default:
             return .requestPlain
