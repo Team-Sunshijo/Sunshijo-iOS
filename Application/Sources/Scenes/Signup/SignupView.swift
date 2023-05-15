0ab120aece3d5b0fb7a21912a1e60b6656f31996
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-05-15 15:29:41 +0900

    ⚡️ :: SignupView

--- Application/Sources/Scenes/Signup/SignupView.swift
+++ Application/Sources/Scenes/Signup/SignupView.swift
@@ -1,9 +1,58 @@
 import SwiftUI
 
 struct SignupView: View {
+    @StateObject var viewModel: SignupViewModel
     var body: some View {
-        Image("Sunshijo")
-            .padding(.vertical, 98)
-        Spacer()
+        ZStack {
+            VStack {
+                Image("Sunshijo")
+                    .padding(.top, 98)
+                Spacer()
+            }
+            VStack {
+                Text("회원가입")
+                    .padding([.top, .bottom], 40)
+                    .font(.system(size: 20, weight: .semibold))
+                AuthTextField(
+                    title: "이름",
+                    placeholeder: "이름 입력",
+                    radius: 5,
+                    border: 1,
+                    text: $viewModel.name
+                )
+                AuthTextField(
+                    title: "아이디",
+                    placeholeder: "아이디 입력",
+                    radius: 5,
+                    border: 1,
+                    text: $viewModel.userId
+                )
+                AuthTextField(
+                    title: "비밀번호",
+                    placeholeder: "비밀번호 입력",
+                    radius: 5,
+                    border: 1,
+                    text: $viewModel.password
+                )
+                AuthTextField(
+                    title: "비밀번호 확인",
+                    placeholeder: "비밀번호 입력",
+                    radius: 5,
+                    border: 1,
+                    text: $viewModel.validPassword
+                )
+                AuthButton(
+                    buttonTitle: "회원가입하기"
+                )
+                .padding(.top, 55)
+                LoginButton(buttonTitle: "로그인하기")
+                Spacer()
+            }
+            .background(SunshijoIOSAsset.Colors.white.swiftUIColor)
+            .cornerRadius(30)
+            .edgesIgnoringSafeArea([.bottom, .top])
+            .padding(.top, 125)
+            .authSetShadow()
+        }
     }
 }
