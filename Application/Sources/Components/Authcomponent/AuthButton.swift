cee8afedbcc03719ad797e8de587400f1d6c0c8e
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-05-17 19:25:49 +0900

    ♻️ :: 텍스트필드에 텍스트가 안 채워졌을시에 색깔변경

--- Application/Sources/Components/Authcomponent/AuthButton.swift
+++ Application/Sources/Components/Authcomponent/AuthButton.swift
@@ -17,12 +17,17 @@ struct AuthButton: View {
         VStack {
             Button(action: action) {
                 Text(buttonTitle)
+                    .frame(maxWidth: .infinity)
+                    .padding([.top, .bottom], 10)
+                    .foregroundColor(SunshijoIOSAsset.Colors.white.swiftUIColor)
+                    .background(
+                        isDisabled ? SunshijoIOSAsset.Colors.main700.swiftUIColor : Color.main
+                    )
             }
-            .frame(height: 40)
-            .padding([.leading, .trailing], 70)
-            .background(SunshijoIOSAsset.Colors.main2.swiftUIColor)
-            .foregroundColor(SunshijoIOSAsset.Colors.white.swiftUIColor)
             .cornerRadius(5)
+            .frame(height: 40)
+            .background(Color.white)
+            .padding([.leading, .trailing], 70)
             .disabled(isDisabled)
         }
     }
