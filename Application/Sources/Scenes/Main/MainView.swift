2e2b3e773354cb1ef2cbbf58bfb24d36fc0529b1
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-05-18 23:26:12 +0900

    🎨 :: mainView에 router추가

--- Application/Sources/Scenes/Main/MainView.swift
+++ Application/Sources/Scenes/Main/MainView.swift
@@ -1,11 +1,10 @@
 import SwiftUI
+import SNavigationAndTab
 
 struct MainView: View {
+    @EnvironmentObject var sunshijoRouter: SunshijoRouter
+    let evaluationView: EvaluationView
     var body: some View {
-        VStack {
-            List() {
-                
-            }
-        }
+
     }
 }
