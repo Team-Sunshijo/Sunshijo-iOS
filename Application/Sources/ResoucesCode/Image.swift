142fe4056504cd32da8686f9a3667d2ff5f5f30d
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-05-22 20:26:56 +0900

    💄 :: ResourceCode image 추가

--- Application/Sources/ResoucesCode/Image.swift
+++ Application/Sources/ResoucesCode/Image.swift
@@ -3,4 +3,9 @@ import SwiftUI
 extension Image {
     // Sunshijo
     static let sunShiJo = Image("Sunshijo")
+    // Tabbar
+    static let timeTable = Image("TimeTable")
+    static let schedule = Image("Schedule")
+    static let evaluation = Image("Evaluation")
+    static let etc = Image("Etc")
 }
