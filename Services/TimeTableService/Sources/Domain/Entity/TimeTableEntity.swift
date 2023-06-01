b437c86171d154be2f82376d0eed45c997762018
juyeong525 <juyeongno1@naver.com>
2023-06-01 20:24:44 +0900

    ⚡️ :: TimeTableEntity

---
+++ Services/TimeTableService/Sources/Domain/Entity/TimeTableEntity.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+public struct TimeTableEntity: Equatable {
+    public let grade: Int
+    public let classNum: Int
+    public let period: Int
+    public let weekOfDate: Int
+    public let subject: String
+}
