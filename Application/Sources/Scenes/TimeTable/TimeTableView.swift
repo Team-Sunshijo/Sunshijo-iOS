1ae4ca7c6ace9426fb225f5640625581b3709d28
juyeong525 <juyeongno1@naver.com>
2023-06-01 20:20:22 +0900

    ⚡️ :: TimeTableView 추가

--- Application/Sources/Scenes/TimeTable/TimeTableView.swift
+++ Application/Sources/Scenes/TimeTable/TimeTableView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 import SNavigationAndTab
 
 struct TimeTableView: View, SNavigationAndTabContent {
-    let viewModel: TimeTableViewModel
+    @StateObject var viewModel: TimeTableViewModel
     var tabInformation: TabInformation {
         TabInformation(
             tabItemText: "시간표",
@@ -12,16 +12,68 @@ struct TimeTableView: View, SNavigationAndTabContent {
     }
     var body: some View {
         VStack(spacing: 0) {
-            Spacer().frame(height: 78)
+            Spacer().frame(minHeight: 30, maxHeight: 78)
             Text("<  이번주 시간표  >")
                 .font(.system(size: 24, weight: .bold))
                 .foregroundColor(Color.gray900)
             Spacer().frame(height: 15)
-            TimeTableComponentView(entity: viewModel.time)
+            VStack(alignment: .leading, spacing: 0) {
+                HStack(spacing: 0) {
+                    Spacer().frame(width: 30, height: 40)
+                    ForEach(viewModel.dayOfTheWeek, id: \.self) {
+                        Text($0)
+                            .font(.system(size: 16, weight: .semibold))
+                            .foregroundColor(Color.gray500)
+                            .frame(width: 56, height: 40)
+                    }
+                }
+                ForEach(0..<4) { index in
+                    HStack(spacing: 0) {
+                        Text("\(index+1)")
+                            .font(.system(size: 16, weight: .regular))
+                            .foregroundColor(Color.gray500)
+                            .frame(width: 30, height: 51)
+                        ForEach(viewModel.timeTable[index], id: \.subject) {
+                            TimeTableCellView(subject: $0.subject)
+                        }
+                    }
+                }
+                HStack {
+                    Spacer()
+                        .frame(width: 30)
+                    VStack(spacing: 0) {
+                        Rectangle().frame(height: 0)
+                        Text("즐거운 점심시간")
+                            .font(.system(size: 20, weight: .medium))
+                            .foregroundColor(Color.white)
+                            .frame(height: 51)
+                    }
+                    .border(Color.gray200)
+                    .frame(width: 280)
+                    .background(Color.main700)
+                }
+                ForEach(4..<7) { index in
+                    HStack(spacing: 0) {
+                        Text("\(index+1)")
+                            .font(.system(size: 16, weight: .regular))
+                            .foregroundColor(Color.gray500)
+                            .frame(width: 30, height: 51)
+                        ForEach(viewModel.timeTable[index], id: \.subject) {
+                            TimeTableCellView(subject: $0.subject)
+                        }
+                    }
+                }
+            }
+            .padding(EdgeInsets(top: 28, leading: 10, bottom: 28, trailing: 10))
+            .background(Color.white)
+            .cornerRadius(8)
+            .shadow(color: .shadowColor, radius: 10, x: 0, y: 2)
             Spacer()
         }
         .padding(.horizontal, 30)
+        .onAppear { viewModel.fetchTimeTable() }
         .onTabSelected(tabIndex: 0, perform: {
+            viewModel.fetchTimeTable()
         })
     }
 }
--- Application/Sources/Scenes/TimeTable/TimeTableViewModel.swift
+++ Application/Sources/Scenes/TimeTable/TimeTableViewModel.swift
@@ -1,5 +1,31 @@
-import SwiftUI
+import Foundation
+
+import RxSwift
+import TimeTableService
 
 class TimeTableViewModel: ObservableObject {
-    var time = ["국어", "국어", "국어", "국어", "국어"]
+    @Published var timeTable: [[TimeTableEntity]] = [[], [], [], [], [], [], []]
+    let dayOfTheWeek = ["월", "화", "수", "목", "금"]
+    private let fetchTimeTableListUseCase: FetchTimeTableListUseCase
+
+    private var disposeBag = DisposeBag()
+
+    init(fetchTimeTableListUseCase: FetchTimeTableListUseCase) {
+        self.fetchTimeTableListUseCase = fetchTimeTableListUseCase
+    }
+
+    func fetchTimeTable() {
+        self.fetchTimeTableListUseCase.excute(grade: 2, classNum: 2)
+            .subscribe(onNext: {
+                var reset: [[TimeTableEntity]] = [[], [], [], [], [], [], []]
+                $0.forEach {
+                    reset[$0.period-1].append($0)
+                }
+                self.timeTable = reset
+            })
+            .disposed(by: disposeBag)
+    }
+    func viewAppear() {
+        self.timeTable = [[], [], [], [], [], [], []]
+    }
 }
