6b067d8c66390f6d0c451067fe9693e3ecf56a38
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-06-02 23:07:27 +0900

    ✨ :: LaunchScreen Add

--- Application/Sources/Scenes/LaunchScreen/LaunchScreenView.swift
+++ Application/Sources/Scenes/LaunchScreen/LaunchScreenView.swift
@@ -1,7 +1,26 @@
 import SwiftUI
 
 struct LaunchScreenView: View {
+
+    @StateObject var viewModel: LaunchScreenViewModel
+    @EnvironmentObject var sunshijoRouter: SunshijoRouter
+
     var body: some View {
         Image("Sunshijo")
+            .onChange(of: viewModel.isSuccessToRefreshToken, perform: { isSuccess in
+                if isSuccess {
+                    self.sunshijoRouter.presentFullScreen(.main)
+                    self.viewModel.isSuccessToRefreshToken = false
+                }
+            })
+            .onChange(of: viewModel.isFailureToRefreshToken, perform: { isFail in
+                self.sunshijoRouter.presentFullScreen(.login)
+                self.viewModel.isFailureToRefreshToken = false
+            })
+            .onChange(of: self.sunshijoRouter.stack.last?.screen, perform: {
+                guard $0 == .launch else { return }
+                viewModel.refreshToken()
+            })
+            .onAppear(perform: viewModel.refreshToken)
     }
 }
