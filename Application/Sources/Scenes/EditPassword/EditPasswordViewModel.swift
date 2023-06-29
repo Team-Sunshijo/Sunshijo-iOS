68c0b95acb208d9d7768834d8d94d03dc25ed9b9
juyeong525 <juyeongno1@naver.com>
2023-06-29 22:49:21 +0900

    ⚡️ :: 더미 추가

--- Application/Sources/Scenes/EditPassword/EditPasswordViewModel.swift
+++ Application/Sources/Scenes/EditPassword/EditPasswordViewModel.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 import RxSwift
 class EditPasswordViewModel: ObservableObject {
-    @Published var currentPassword: String = ""
+    @Published var currentPassword: String = "test1234**"
     @Published var newPassword: String = ""
     @Published var newPasswordCheck: String = ""
     private var disposebag = DisposeBag()
--- Application/Sources/Scenes/Profile/ProfileViewModel.swift
+++ Application/Sources/Scenes/Profile/ProfileViewModel.swift
@@ -1,9 +1,9 @@
 import SwiftUI
 import RxSwift
 class ProfileViewModel: ObservableObject {
-    @Published var name: String = ""
-    @Published var userId: String = ""
-    @Published var classNum: String = ""
+    @Published var name: String = "박도연"
+    @Published var userId: String = "test1234"
+    @Published var classNum: String = "2학년 2반"
     private var disposebag = DisposeBag()
 
 }
